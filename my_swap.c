008135f3932ba801bb30e079459f4f7b2bb50db7
Mahmoud Ismail <ismailm7373@gmail.com>
2024-03-28 09:09:29 +0000

    task

--- my_swap.c
+++ my_swap.c
@@ -7,6 +7,27 @@
  */
 void my_swap(stack_t **stack, unsigned int line_number)
 {
-	(void) stack;
-	(void) line_number;
+	stack_t *p = *stack;
+	int i;
+
+	i = 0;
+	while(p)
+	{
+		p = p->prev;
+		i++;
+	}
+	if (i < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		stack_t *p = *stack;
+
+		p = p->prev;
+		i = p->n;
+		p->n = (*stack)->n;
+		(*stack)->n = i;
+	}
 }
