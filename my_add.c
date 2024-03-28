c29a0a26a3050106ecc647c44f2d9ae114bba64b
Mahmoud Ismail <ismailm7373@gmail.com>
2024-03-28 09:20:18 +0000

    task

--- my_add.c
+++ my_add.c
@@ -7,6 +7,27 @@
  */
 void my_add(stack_t **stack, unsigned int line_number)
 {
-	(void) stack;
-	(void) line_number;
+	stack_t *p = *stack;
+	int i;
+
+	i = 0;
+	while(p)
+	{
+		p = p->prev;
+		i++;
+	}
+	if (i < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		stack_t *p = *stack;
+		i = p->n + (p->prev)->n;
+		*stack = (*stack)->prev;
+		(*stack)->n = i;
+		(*stack)->next = NULL;
+		free(p);
+	}
 }
--- my_pop.c
+++ my_pop.c
@@ -17,6 +17,7 @@ void my_pop(stack_t **stack, unsigned int line_number)
 		stack_t *p = *stack;
 
 		*stack = (*stack)->prev;
+		(*stack)->next = NULL;
 		free(p);
 	}
 }
