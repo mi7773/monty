3257b9c5a84c4117040d645a4d5c6cbe2c748f41
Mahmoud Ismail <ismailm7373@gmail.com>
2024-03-28 11:16:28 +0000

    task

--- my_getline.c
+++ my_getline.c
@@ -11,16 +11,13 @@ char *my_getline(FILE *fp, int i)
 	size_t n = 0;
 	ssize_t r;
 	char *lineptr = 0;
+	(void) i;
 
-	while (i)
+	r = getline(&lineptr, &n, fp);
+	if (r == -1)
 	{
-		r = getline(&lineptr, &n, fp);
-		if (r == -1)
-		{
-			free(lineptr);
-			return (0);
-		}
-		i--;
+		free(lineptr);
+		return (0);
 	}
 	return (lineptr);
 }
