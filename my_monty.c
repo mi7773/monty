47c1c7695cb615435a502e48d2823e55200da52c
Mahmoud Ismail <ismailm7373@gmail.com>
2024-03-28 20:22:43 +0000

    task

--- my_monty.c
+++ my_monty.c
@@ -16,39 +16,30 @@ int d = 0;
 void my_monty(instruction_t *instruction, char *lineptr, int line_number,
 		stack_t **stack, FILE *fp, char *av1)
 {
-	size_t l = strlen(lineptr) + 1;
-	char *c = (char *) malloc(l * sizeof(char));
-	char *p = NULL;
+	char *p = strtok(lineptr, " \n");
 	int i;
 
 	i = 0;
 	while (i <= 6)
 	{
-		c = strcpy(c, lineptr);
-		p = strtok(c, " \n");
-		while (p)
+		if (strcmp(instruction[i].opcode, p) == 0)
 		{
-			if (strcmp(instruction[i].opcode, p) == 0)
+			if (i == 0)
 			{
-				if (i == 0)
+				p = strtok(0, " \n");
+				if (p)
 				{
-					p = strtok(0, " \n");
-					if (p)
-					{
-						d = atoi(p);
-					}
+					d = atoi(p);
 				}
-				my_free(0, c, lineptr, 0, av1);
-				instruction[i].f(stack, line_number);
-				d = 0;
-				return;
 			}
-			p = strtok(0, " \n");
+			my_free(0, 0, lineptr, 0, av1);
+			instruction[i].f(stack, line_number);
+			d = 0;
+			return;
 		}
 		i++;
 	}
-	p = strtok(lineptr, " \n");
 	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, p);
-	my_free(stack, c, lineptr, fp, av1);
+	my_free(stack, 0, lineptr, fp, av1);
 	exit(EXIT_FAILURE);
 }
