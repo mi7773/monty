797bc9c4bac8b120c881f6c8d1520a28ccff1886
Mahmoud Ismail <ismailm7373@gmail.com>
2024-03-28 11:30:15 +0000

    task

--- my_pop.c
+++ my_pop.c
@@ -9,7 +9,7 @@ void my_pop(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	else
