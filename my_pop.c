3923a7f7b8c984e01925c525d937e2eb5c366b67
Mahmoud Ismail <ismailm7373@gmail.com>
2024-03-28 08:51:40 +0000

    task

--- my_pop.c
+++ my_pop.c
@@ -7,6 +7,16 @@
  */
 void my_pop(stack_t **stack, unsigned int line_number)
 {
-	(void) stack;
-	(void) line_number;
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		stack_t *p = *stack;
+
+		*stack = (*stack)->prev;
+		free(p);
+	}
 }
